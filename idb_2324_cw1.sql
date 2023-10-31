c8db6203f97f2dbae4190f2b2bc48ef127c8df1d
Konstantinos Gkoutzis <kgk@ic.ac.uk>
2023-10-31 11:43:50 +0000

    Original skeleton

---
+++ idb_2324_cw1.sql
@@ -0,0 +1,40 @@
+-- Q1 returns (name,born_in,father,mother)
+
+;
+
+-- Q2 returns (name)
+
+;
+
+-- Q3 returns (name)
+
+;
+
+-- Q4 returns (house,name,accession)
+
+;
+
+-- Q5 returns (name,role,start_date)
+
+;
+
+-- Q6 returns (first_name,popularity)
+
+;
+
+-- Q7 returns (party,seventeenth,eighteenth,nineteenth,twentieth,twentyfirst)
+
+; 
+
+-- Q8 returns (mother,child,born)
+
+;
+
+-- Q9 returns (monarch,prime_minister)
+
+;
+       
+-- Q10 returns (name,entry,period,days)
+
+;
+
